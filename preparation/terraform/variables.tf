f3d94f0eb1df8a37cc13953d3d2289f5e8db18cf
adysulton <adysulton@gmail.com>
2024-02-27 13:44:52 +0700

    create vm using terraform

---
+++ preparation/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  default = "./service_account.json"
+  type    = string
+}
+
+variable "project" {
+  default = "graphite-scout-414507"
+  type    = string
+}
+
+variable "region" {
+  default = "asia-southeast2"
+  type    = string
+}
+
+variable "zone" {
+  default = "asia-southeast2-b"
+  type    = string
+}
+
+variable "email" {
+  default = "de-zoomcamp@graphite-scout-414507.iam.gserviceaccount.com"
+  type    = string
+}
+
+variable "machine_type" {
+  default = "e2-standard-2"
+  type = string
+}
+
+variable "disk_size" {
+  default = 20
+  type = number
+}
